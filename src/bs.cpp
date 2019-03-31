2c41d4e595fc1447daa14f099fe5ff0ea1738b64
Ali Khudiyev <xudiyev-ali@mail.ru>
2019-03-31 16:01:19 +0400

    new commit

--- src/bs.cpp
+++ src/bs.cpp
@@ -7,14 +7,6 @@ int main(int argc, char* argv[]){
 
     User Ali("Ali");
     User Kanan("Kanan");
-    
-    while(1){
-        if(Ali.play(Kanan)==2) break; // ==1
-        if(Kanan.play(Ali)==2) break; // ==2
-    }
-
-    Ali.is_defeated();
-    Kanan.is_defeated();
 
     return 0;
 }
